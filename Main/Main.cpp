7636dc5db85b7997a05baa584c66249f57f17959
lixy <lixy@vipcode.com>
2020-11-05 17:53:04 +0800

    重新整理文件结构

---
+++ Main/Main.cpp
@@ -0,0 +1,13 @@
+// algorithm.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    system("pause");
+	return 0;
+}
+
